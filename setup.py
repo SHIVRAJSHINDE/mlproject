9dd466479141935fadd9a5025d484875be6b07d3
SHIVRAJ SHINDE <shindeshivraaj@gmail.com>
2023-03-10 19:32:50 +0530

    with setup file

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import find_packages,setup
+
+setup(
+    name='mlproject',
+    version='0.0.1',
+    author='SHIVRAJ SHINDE',
+    author_email='shindeshivraaj@gmail.com',
+    packages=find_packages(),
+    install_requires= ['pandas','numpy','seaborn']
+    
+)
