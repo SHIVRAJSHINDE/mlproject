71572c9c1dfa83c12f1b0ccf7586f0b2fc8cb32f
SHIVRAJ SHINDE <shindeshivraaj@gmail.com>
2023-03-13 11:50:58 +0530

    with data ingestion file

--- src/exception.py
+++ src/exception.py
@@ -8,7 +8,7 @@ def error_messages_detail(error,error_details:sys):
     return error_message
 
     
-class CustomeExceptoin(Exception):
+class CustomException(Exception):
     def __init__(self,error_message,error_detail:sys):
         super().__init__(error_message)
         self.error_message = error_messages_detail(error_message,error_details=error_detail)
@@ -16,11 +16,3 @@ class CustomeExceptoin(Exception):
         
     def __str__(self):
         return self.error_message
-
-
-if __name__ == "__main__":
-    try:
-        a=1/0
-    except Exception as e:
-        logging.info("Divided by Zero")
-        raise CustomeExceptoin(e,sys)
